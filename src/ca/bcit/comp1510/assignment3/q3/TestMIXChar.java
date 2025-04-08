0cbd62521a7a12480dd3e122260d20724c501487
SowinskiBraeden <braeden@sowinski.dev>
2025-04-07 17:51:54 -0700

    update MIXChar

--- src/ca/bcit/comp1510/assignment3/q3/TestMIXChar.java
+++ src/ca/bcit/comp1510/assignment3/q3/TestMIXChar.java
@@ -1,5 +1,45 @@
 package ca.bcit.comp1510.assignment3.q3;
 
-public class TestMixChar {
+import java.util.Scanner;
 
+/**
+ * TestMIXChar test MIXChar and Message.
+ * @author Braeden Sowinski
+ * @version 1.0.0
+ */
+public class TestMIXChar {
+
+    /**
+     * main program entry.
+     * @param args unused
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Enter a message: ");
+        String line = scan.nextLine();
+                
+        scan.close();
+
+        char[] c = line.toCharArray();
+        MIXChar[] characters = new MIXChar[c.length];
+        
+        for (int i = 0; i < c.length; i++) {
+            try {
+                characters[i] = new MIXChar(c[i]);
+            } catch (IllegalArgumentException e) {
+                System.out.println(
+                    "\"" 
+                    + line 
+                    + "\" contains an invalid MIX Character"
+                );
+                return;
+            }
+        }
+        
+        Message m = new Message(characters);
+        
+        System.out.println(m.toLongs());
+        System.out.println(m.toString());
+    }
 }
